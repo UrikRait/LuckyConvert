bb425c6be2d3d9bc6157f50c08c6b8bc1db6e10a
UrikRait <123385837+UrikRait@users.noreply.github.com>
2024-10-02 16:16:22 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -2,22 +2,20 @@ from PIL import Image
 import os
 
 def convert_to_webp(input_file, output_file, quality=85):
-    # Открываем изображение
+    
     img = Image.open(input_file)
     
-    # Конвертируем изображение в WebP и сохраняем его
     img.save(output_file, 'WEBP', quality=quality)
     
-    # Удаляем оригинальный файл
     os.remove(input_file)
     print(f"Converted {os.path.basename(input_file)} to {os.path.basename(output_file)} and deleted original.")
 
 if __name__ == "__main__":
-    folder = os.getcwd()  # Текущая папка, где лежит скрипт
+    folder = os.getcwd() 
 
-    # Проходим по каждому файлу в папке
+    
     for filename in os.listdir(folder):
-        if filename.endswith(('.png', '.jpg', '.jpeg')):  # Проверка форматов
+        if filename.endswith(('.png', '.jpg', '.jpeg')):  
             input_file = os.path.join(folder, filename)
             output_file = os.path.join(folder, f"{os.path.splitext(filename)[0]}.webp")
             convert_to_webp(input_file, output_file)
